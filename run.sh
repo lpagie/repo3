1e15c6964702d9a70572b203bb8edf9f3125d49b
Ludo Pagie <l.pagie@nki.nl>
2021-10-06 17:24:02 +0200

    update path in run.sh to repo3

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # execute this run.sh one directory above this repo directory
 
-snakemake -prs repo1/snakefile_1.smk -c1 --configfile repo1/config.yaml
+snakemake -prs repo3/snakefile_1.smk -c1 --configfile repo3/config.yaml
